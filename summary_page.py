1739efe4055b954dc9a4f98be8ee7896fa148229
“muoegu” <kmicah1794@gmail.com>
2025-03-28 16:42:43 +0800

    update

--- summary_page.py
+++ summary_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 def summary_page():
     st.title("Summary")
 
-
+    st.image("jia.png")
     st.image("pipeline.png", caption="Pipeline")
     st.image("prompt.png", caption="Prompt")
     
